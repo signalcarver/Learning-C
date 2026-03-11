363f97228c3924406e6f1ff6c0b7c0860b952bf4
The Reason Of Intricacy <aikasai.co@gmail.com>
2026-03-11 02:22:53 -0400

    Add files via upload
    A mini project of a battery runtime tester, can change the battery watt size.

---
+++ Batterytest.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    // variables
+    double battery;
+    double consumption;
+    double runtime;
+
+        printf("Enter your battery percentage:");
+        scanf("%lf", &battery);
+
+        printf("Enter your consumtions (Watts):");
+        scanf("%lf", &consumption);
+
+    // Calculation for result (the *500 is assuming the battery size adjust to right size)
+
+    runtime = (battery / 100.0) * 500 / consumption;
+    
+    // The Result for the use  
+        printf("runtime: %.2lf Hours\n", runtime);
+        
+        if (battery > 75) {
+        printf("Battery good\n");
+    } else if (battery > 30) {
+        printf("Battery OK - consider charging soon\n");
+    } else if (battery > 10) {
+        printf("Battery low - charge now\n");
+    } else {
+        printf("Critical - shutting down\n");
+    }
+
+
+        return 0;
+}
+
